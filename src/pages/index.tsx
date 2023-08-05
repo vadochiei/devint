6bc2f4326a855e4a168e702c8d510faefd4009e8
vadochiei <vlad.adochiei@yahoo.com>
2023-08-05 15:19:48 +0300

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <Navigation />
       <Hero />
       <SectionTitle
-        pretitle="Nextly Benefits"
+        pretitle="Devint Benefits"
         title=" Why should you use this landing page">
         Nextly is a free landing page & marketing website template for startups
         and indie projects. Its built with Next.js & TailwindCSS. And its
